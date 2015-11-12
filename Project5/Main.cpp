ab683ee84fe2272de25f51294d7d1fe32c10e4a4
Taylor Vaughn <tev1@rice.edu>
2015-11-12 17:04:53 -0600

    Init for project 5

---
+++ Project5/Main.cpp
@@ -0,0 +1,101 @@
+#include "Main.h"
+
+using namespace ompl;
+
+base::ValidStateSamplerPtr allocUniformStateSampler(const base::SpaceInformation *si)
+{
+    return base::ValidStateSamplerPtr(new base::UniformValidStateSampler(si));
+}
+
+
+
+// This is our state validity checker for checking if our point robot is in collision
+bool isValidStatePoint(const ompl::base::State* state, const std::vector<Rectangle>& obstacles)
+{
+    const ompl::base::RealVectorStateSpace::StateType* r2state;
+    r2state = state->as<ompl::base::RealVectorStateSpace::StateType>();
+    // Extract x, y
+    double x = r2state->values[0];
+    double y = r2state->values[1];
+
+    return isValidPoint(x, y, obstacles);
+}
+
+// This is our state validity checker.  It says every state is valid.
+bool stateAlwaysValid(const ompl::base::State* /*state*/)
+{
+	//std::cout << "HERE!\n" ;   
+	return true;
+}
+
+
+int main(int, char **)
+{
+	// Empty environment
+	std::vector<Rectangle> none;
+
+	//Street environment
+    std::vector<Rectangle> obstacles;
+    Rectangle obstacle;
+
+	//1
+    obstacle.x = -10.0;
+    obstacle.y = -10.0;
+    obstacle.width = 20.0;
+    obstacle.height = 4.0;
+    obstacles.push_back(obstacle);
+
+	//2
+	obstacle.x = -10;
+	obstacle.y = -4;
+	obstacle.width = 10;
+	obstacle.height = 8;
+	obstacles.push_back(obstacle);
+
+	//3
+	obstacle.x = 2;
+	obstacle.y = -4;
+	obstacle.width = 8;
+	obstacle.height = 8;
+	obstacles.push_back(obstacle);
+
+	//4
+	obstacle.x = -10;
+	obstacle.y = 6;
+	obstacle.width = 20;
+	obstacle.height = 2;
+	obstacles.push_back(obstacle);
+
+
+	int benchmarkOrPlan;
+	do 
+	{
+        std::cout << "Benchmark (1) or Plan (2) "<< std::endl;
+		std::cin >> benchmarkOrPlan;
+
+	} while (benchmarkOrPlan < 1 || benchmarkOrPlan > 2);
+
+	switch (benchmarkOrPlan) 
+	{
+		case BENCHMARK:
+			int benchmarkChoice;
+			do
+			{
+				std::cout << "Benchmark for: "<< std::endl;
+				std::cout << " (1) Pendulum" << std::endl;
+				std::cout << " (2) Car" << std::endl;
+				
+				std::cin >> benchmarkChoice;
+			} while (benchmarkChoice < 1 || benchmarkChoice > 2);
+	    
+			break;
+
+		case PLANNER:
+			std::cout << "Running in street like environment\n";
+			planWithSimpleSetupCar(obstacles, -10, 10, -10, 10, -5, -5, 5, 5, 1);
+			break;
+			
+	}
+    return 0;
+}
+
---
+++ Project5/Main.h
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <boost/bind.hpp>
+
+
+
+// Including SimpleSetup.h will pull in MOST of what you need to plan
+#include <ompl/base/samplers/UniformValidStateSampler.h>
+
+// Except for the state space definitions and any planners
+#include <omplapp/apps/SE3RigidBodyPlanning.h>
+
+#include <ompl/control/planners/rrt/RRT.h>
+#include "SMR.h"
+#include <ompl/control/planners/kpiece/KPIECE1.h>
+
+#include <ompl/control/SpaceInformation.h>
+#include <ompl/base/spaces/SE2StateSpace.h>
+#include <ompl/control/ODESolver.h>
+#include <ompl/control/spaces/RealVectorControlSpace.h>
+#include <ompl/control/SimpleSetup.h>
+#include <ompl/config.h>
+#include <iostream>
+#include <fstream>
+#include <valarray>
+#include <limits>
+
+
+
+// The collision checker produced in project 2
+#include "CollisionChecking.h"
+
+
+const int BENCHMARK  = 1;
+const int PLANNER = 2;
+
+const int PENDULUM  = 1;
+const int CAR = 2;
+
+const int RRT = 1;
+const int KPIECE = 2;
+const int RGRRT = 3;
+
+
+
+void planWithSimpleSetupCar(const std::vector<Rectangle>& obstacles,  int low, int high, int clow, int chigh, double startX, double startY, double goalX, double goalY, int plannerChoice);
+
+void CarODE (const ompl::control::ODESolver::StateType& q, const ompl::control::Control* control, ompl::control::ODESolver::StateType& qdot);
+
+void CarPostIntegration (const ompl::base::State* /*state*/, const ompl::control::Control* /*control*/, const double /*duration*/, ompl::base::State *result);
+
+ompl::base::ValidStateSamplerPtr allocUniformStateSampler(const ompl::base::SpaceInformation *si);
+
+
+bool stateAlwaysValid(const ompl::base::State* /*state*/);
+
+
+bool isStateValid(const ompl::control::SpaceInformation *si, const ompl::base::State *state, const std::vector<Rectangle>& obstacles);
+bool isStateValid(const ompl::control::SpaceInformation *si, const ompl::base::State *state);
+
+bool isValidStatePoint(const ompl::base::State* state, const std::vector<Rectangle>& obstacles);
+
+
+class CarControl : public ompl::control::Control {
+
+	public:
+		CarControl() {}
+};
+
